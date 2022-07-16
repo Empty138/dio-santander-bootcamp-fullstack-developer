586c0bae001136fceaac058eab362bd93fa6e901
Lucas Silva <lucas.rosilva@gmail.com>
2022-07-16 19:19:34 -0300

    debugging

---
+++ 5-java/intellij/debugging-java/src/br/com/dio/debugging/CalculadoraDeMedias.java
@@ -0,0 +1,22 @@
+package br.com.dio.debugging;
+ import java.util.Scanner;
+
+public class CalculadoraDeMedias {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Camila", "Lucas", "Bruna", "Prdro"};
+
+        int media = calcularMediaDaTurma(alunos, scan);
+        System.out.printf("Média da turma %d", media);
+    }
+
+    public static int calcularMediaDaTurma(String[] alunos, Scanner scanner) {
+       int soma = 0;
+       for(String aluno : alunos) {
+           System.out.printf("Nota do aluno %s: ", aluno);
+           int nota = scanner.nextInt();
+           soma += nota;
+       }
+       return soma / alunos.length;
+    }
+}
