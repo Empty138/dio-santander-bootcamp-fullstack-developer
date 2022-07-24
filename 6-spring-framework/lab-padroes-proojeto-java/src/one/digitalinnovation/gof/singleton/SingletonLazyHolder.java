edd7f04e96c7a28364717f4b87d1c601f4fcdcee
Lucas Silva <lucas.rosilva@gmail.com>
2022-07-24 16:50:56 -0300

    desafio-padroes-projeto

---
+++ 6-spring-framework/lab-padroes-proojeto-java/src/one/digitalinnovation/gof/singleton/SingletonLazyHolder.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.gof.singleton;
+
+/**
+ * Singleton "Lazy Holder"
+ *
+ * @see <a href="https://stackoverflow.com/a/24018148">Referencia</a>
+ *
+ * @author Empty138
+ */
+public class SingletonLazyHolder {
+    private static  class InstanceHolder {
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+    private SingletonLazyHolder() {
+        super();
+    }
+
+    public static SingletonLazyHolder getInstance() {
+        return InstanceHolder.instancia;
+    }
+}
